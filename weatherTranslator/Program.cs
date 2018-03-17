1fe572dd401d257310c92e24a72d5b9f9aa53ae7
Vladimir Zotov <zott14@outlook.com>
2018-03-17 12:12:37 +0300

    Update

--- weatherTranslator/Program.cs
+++ weatherTranslator/Program.cs
@@ -11,71 +11,38 @@ namespace weatherTranslator
         static void Main(string[] args)
         {
 
-            Console.Write("Введите какое-либо слово о погоде, и я попробую перевести его на английский: ");
+            Console.WriteLine("Введите какое-либо слово о погоде на русском языке: ");
             string userWord = Console.ReadLine();
 
             switch(userWord)
-            {
-                case "дождь":
-                    {
-                        Console.WriteLine("rain");
-                        break;
-                    }
-                case "снег":
-                  {
-                        Console.WriteLine("snow");
-                        break;
-                    }
-                case "гроза":
-                    {
-                        Console.WriteLine("storm");
-                        break;
-                    }
-                case "жара":
-                    {
-                        Console.WriteLine("heat");
-                        break;
-                    }
-                case "холод":
-                    {
-                        Console.WriteLine("cold");
-                        break;
-                    }
-                case "мороз":
-                    {
-                        Console.WriteLine("frost");
-                        break;
-                    }
-                case "облако":
-                    {
-                        Console.WriteLine("cloud");
-                        break;
-                    }
-                case "туча":
-                    {
-                        Console.WriteLine("cloud");
-                        break;
-                    }
-                case "ветер":
-                    {
-                        Console.WriteLine("wind");
-                        break;
-                    }
-                case "метель":
-                    {
-                        Console.WriteLine("blizzard");
-                        break;
-                    }
-                case "град":
-                    {
-                        Console.WriteLine("hail");
-                        break;
-                    }
+            { 
+                case "дождь": Console.WriteLine("rain");
+                    break;
+                case "снег":  Console.WriteLine("snow");
+                    break;                   
+                case "гроза": Console.WriteLine("storm");
+                    break;                   
+                case "жара":  Console.WriteLine("heat");
+                    break;                    
+                case "холод": Console.WriteLine("cold");
+                    break;                  
+                case "мороз": Console.WriteLine("frost");
+                    break;                    
+                case "облако": Console.WriteLine("cloud");
+                    break;
+                case "туча": Console.WriteLine("cloud");
+                    break;                   
+                case "ветер": Console.WriteLine("wind");
+                    break;                   
+                case "метель": Console.WriteLine("blizzard");
+                    break;                   
+                case "град": Console.WriteLine("hail");
+                    break;
+                    
                 default:
-                    {
-                        Console.WriteLine("Такое слово я пока не знаю.");
-                        break;
-                    }
+                    Console.WriteLine("Такое слово я пока не знаю.");
+                    break;
+                    
             }
 
             Console.ReadKey();
